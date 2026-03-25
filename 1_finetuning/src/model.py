be2c2da3e5fec055223dd1fd3d8adb44fe0c1119
Luiza Bretas <80011623+ArthLoki@users.noreply.github.com>
2026-03-24 23:41:08 -0300

    fix: black .

--- 1_finetuning/src/model.py
+++ 1_finetuning/src/model.py
@@ -1,9 +1,12 @@
 from unsloth import FastLanguageModel
+
 # import torch
 
-max_seq_length = 2048 # Choose any! We auto support RoPE Scaling internally!
-dtype = None # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-load_in_4bit = True # Use 4bit quantization to reduce memory usage. Can be False.
+max_seq_length = 2048  # Choose any! We auto support RoPE Scaling internally!
+dtype = (
+    None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
+)
+load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
 
 
 def getMaxSeqLength():
@@ -25,25 +28,33 @@ def getBaseModelAndTokenizer():
     # ] # More models at https://huggingface.co/unsloth
 
     model, tokenizer = FastLanguageModel.from_pretrained(
-        model_name = "unsloth/Llama-3.2-3B-Instruct",
-        max_seq_length = max_seq_length,
-        dtype = dtype,
-        load_in_4bit = load_in_4bit,
+        model_name="unsloth/Llama-3.2-3B-Instruct",
+        max_seq_length=max_seq_length,
+        dtype=dtype,
+        load_in_4bit=load_in_4bit,
         # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
     )
 
     model = FastLanguageModel.get_peft_model(
         model,
-        r = 16, # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-        target_modules = ["q_proj", "k_proj", "v_proj", "o_proj", "gate_proj", "up_proj", "down_proj",],
-        lora_alpha = 16,
-        lora_dropout = 0, # Supports any, but = 0 is optimized
-        bias = "none",    # Supports any, but = "none" is optimized
+        r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+        lora_alpha=16,
+        lora_dropout=0,  # Supports any, but = 0 is optimized
+        bias="none",  # Supports any, but = "none" is optimized
         # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-        use_gradient_checkpointing = "unsloth", # True or "unsloth" for very long context
-        random_state = 3407,
-        use_rslora = False,  # We support rank stabilized LoRA
-        loftq_config = None, # And LoftQ
+        use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
+        random_state=3407,
+        use_rslora=False,  # We support rank stabilized LoRA
+        loftq_config=None,  # And LoftQ
     )
 
     return (model, tokenizer)
@@ -53,7 +64,10 @@ def save_model():
     try:
         model, tokenizer = getBaseModelAndTokenizer()
         # save_lora_model(model, tokenizer)
-        model.save_pretrained_gguf("model", tokenizer,)
+        model.save_pretrained_gguf(
+            "model",
+            tokenizer,
+        )
         return True
     except Exception as err:
         print(f"Error while saving model: {err}")
